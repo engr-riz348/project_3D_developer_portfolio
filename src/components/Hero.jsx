7566fa065a35727e99d0801d866183b055a8630c
engr-riz348 <rizwan.ahmed664@yahoo.com>
2025-09-26 10:54:11 +0500

    chages updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,25 +2,26 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { profile } from "../assets";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[160px] z-10 max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[100px] z-10 max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div className="w-full max-w-2xl">
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Rizwan Ahmed</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 leading-relaxed`}>
-            Software Engineering student who builds, designs, and secures web applications.
-          </p>
+        <div className="w-full max-w-7xl flex flex-row items-center justify-between gap-20">
+          <div className="flex-1">
+            <h1 className={`${styles.heroHeadText} text-white -mt-12`}>
+              Hi, I'm <span className='text-[#915EFF]'>Rizwan Ahmed</span>
+            </h1>
+          </div>
+          <img src={profile} alt="Rizwan Ahmed Profile" className="w-[400px] h-[400px] rounded-lg object-contain shadow-2xl ml-auto mr-[-6rem] mt-6 flex-shrink-0" />
         </div>
       </div>
 
